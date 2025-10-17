ce48b096358dd0f001f287cba8b417fc3e42f389
sH <castsh0104@gmail.com>
2025-10-17 19:41:01 +0900

    Initial project setup with CSS and Firebase separation

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // .tsx 파일도 읽도록 설정
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
